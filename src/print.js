dfbc4cf21f234bcff74a4fade5ce90b0aeeecc9e
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-24 15:41:59 -0500

    Update Structure
    -Add linter files
    -Add src and dist folders
    -Add boiler plate for markup and script
    -Add images directory
    -Add style sheet

---
+++ src/print.js
@@ -0,0 +1,19 @@
+
+// const list = document.querySelector('.todoItems');
+
+// export const todoUpdate = (toDo, id, done) => {
+//   const item = `
+//     <li class="todo-task" ${id}>
+//       <input type="checkbox" class="checkbox" ${done ? 'checked' : ''} id="${id}"/>
+//       <input class="input" type="text" value='${toDo}' id="${id}" readonly/>
+//       <img src="https://img.icons8.com/wired/64/000000/filled-trash.png" alt="delete" class="delete" id="${id}"/>
+//       <img src="https://img.icons8.com/material-outlined/24/000000/menu-2.png" alt="Move Item" class="dotsMenu" id="${id}" />
+//     </li> `;
+
+//   list.insertAdjacentHTML('beforeend', item);
+// };
+
+
+
+
+
