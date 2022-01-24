371ed1e2681484ecbfd44ec9da77040ad5e98281
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-24 18:26:09 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -27,12 +27,11 @@ const myList = [
 
 let htmlList = '';
 
-myList.forEach ((item) => {
-  htmlList+=
-  `<li class="input-li"><input type="checkbox" name="inputDone" id="inputDone">
+myList.forEach((item) => {
+  htmlList +=`<li class="input-li"><input type="checkbox" name="inputDone" id="inputDone">
     <p class="item-edit" id="${item.id}">${item.listItem}</p>
     <img class="grab" src="https://img.icons8.com/material-outlined/24/000000/menu-2.png" alt="grab">
-  </li>;`
+  </li>`;
 });
 
 list.innerHTML = htmlList;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
 
 module.exports = {
   mode: 'development',
@@ -11,7 +11,7 @@ module.exports = {
     static: './dist',
   },
   output: {
-    filename: 'bundle.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
