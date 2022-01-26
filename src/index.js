7fbbeab00dbd1d6fffdb687f6c34289899a7ca5e
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-26 09:24:25 -0500

    Fixed linter error
    Remove  plugin from import.js

--- src/index.js
+++ src/index.js
@@ -1,26 +1,24 @@
 import './style.css';
 
-
-let userInput = [];
+const userInput = [];
 
 function renderTodo(todo) {
   const list = document.querySelector('.form-items');
-  
-  const isCompleted = todo.completed ? 'complete': '';
 
-  const htmlLi = document.createElement("li");
+  const isCompleted = todo.completed ? 'complete' : '';
+
+  const htmlLi = document.createElement('li');
   htmlLi.setAttribute('class', `todo-item ${isCompleted}`);
-  htmlLi.classList.add('input-li')
+  htmlLi.classList.add('input-li');
   htmlLi.setAttribute('data-key', todo.id);
 
   htmlLi.innerHTML = `
   <input id="${todo.id}" type="checkbox" />
   <p class="item-edit" id="${todo.id}">${todo.listItem}</p>
   <img class="grab" src="https://img.icons8.com/material-outlined/24/000000/menu-2.png" alt="grab">`;
-  
+
   list.append(htmlLi);
 }
-
 
 function addTodo(text) {
   const todo = {
@@ -28,14 +26,14 @@ function addTodo(text) {
     completed: false,
     id: Date.now(),
   };
-  
+
   userInput.push(todo);
   renderTodo(todo);
 }
 
 const form = document.querySelector('.input-container');
 
-form.addEventListener('submit', event => {
+form.addEventListener('submit', (event) => {
   event.preventDefault();
   const input = document.querySelector('.input');
   const text = input.value.trim();
