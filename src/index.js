7d0859eb250c869f29c23fb31732611ca81118eb
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-26 05:25:11 -0500

    Feat: add new todo item in script

--- src/index.js
+++ src/index.js
@@ -1,22 +1,47 @@
 import './style.css';
 
-const list = document.querySelector('.form-items');
 
-const myList = [
-  {
-    listItem: '',
+let userInput = [];
+
+function renderTodo(todo) {
+  const list = document.querySelector('.form-items');
+  
+  const isCompleted = todo.completed ? 'complete': '';
+
+  const htmlLi = document.createElement("li");
+  htmlLi.setAttribute('class', `todo-item ${isCompleted}`);
+  htmlLi.classList.add('input-li')
+  htmlLi.setAttribute('data-key', todo.id);
+
+  htmlLi.innerHTML = `
+  <input id="${todo.id}" type="checkbox" />
+  <p class="item-edit" id="${todo.id}">${todo.listItem}</p>
+  <img class="grab" src="https://img.icons8.com/material-outlined/24/000000/menu-2.png" alt="grab">`;
+  
+  list.append(htmlLi);
+}
+
+
+function addTodo(text) {
+  const todo = {
+    listItem: text,
     completed: false,
-    id: 1,
-  },
-];
+    id: Date.now(),
+  };
+  
+  userInput.push(todo);
+  renderTodo(todo);
+}
 
-let htmlList = '';
+const form = document.querySelector('.input-container');
 
-myList.forEach((item) => {
-  htmlList += `<li class="input-li"><input type="checkbox" name="inputDone" id="inputDone">
-    <p class="item-edit" id="${item.id}">${item.listItem}</p>
-    <img class="grab" src="https://img.icons8.com/material-outlined/24/000000/menu-2.png" alt="grab">
-  </li>`;
+form.addEventListener('submit', event => {
+  event.preventDefault();
+  const input = document.querySelector('.input');
+  const text = input.value.trim();
+  if (text !== '') {
+    addTodo(text);
+    input.value = '';
+    input.focus();
+  }
 });
-
-list.innerHTML = htmlList;
