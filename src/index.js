afbe6f1c6492a63516c687f1f9390b52b17283c7
Steve <81635839+steveWDamesJr@users.noreply.github.com>
2022-01-27 20:22:45 -0500

    Feat: edit
    Add edit function in script
    Add save edited information to local storage function

--- src/index.js
+++ src/index.js
@@ -1,23 +1,42 @@
 import './style.css';
 
-const userInput = [];
+let userInput = [];
 
 function renderTodo(todo) {
+  localStorage.setItem('todoItemsRef', JSON.stringify(userInput));
+  // localStorage.setItem('task', JSON.stringify(userInput));
   const list = document.querySelector('.form-items');
+  const item = document.querySelector(`[data-key='${todo.id}']`);
+
+  if (todo.deleted) {
+    item.remove();
+    if (userInput.length === 0) list.innerHTML = '';
+    return;
+  }
 
   const isCompleted = todo.completed ? 'complete' : '';
-
   const htmlLi = document.createElement('li');
-  htmlLi.setAttribute('class', `todo-item ${isCompleted}`);
-  htmlLi.classList.add('input-li');
+
+  htmlLi.setAttribute('class', `input-li ${isCompleted}`);
+  // htmlLi.classList.add('input-li');
   htmlLi.setAttribute('data-key', todo.id);
-
   htmlLi.innerHTML = `
-  <input id="${todo.id}" type="checkbox" />
-  <p class="item-edit" id="${todo.id}">${todo.listItem}</p>
-  <img class="grab" src="https://img.icons8.com/material-outlined/24/000000/menu-2.png" alt="grab">`;
+  <input id="${todo.id}" type="checkbox"/>
+  <label class="ticked js-tick" for="${todo.id}"></label>
+  
+  
+      <p class="task input-li item-edit render-item" id="${todo.id}">${todo.listItem}</p>
 
-  list.append(htmlLi);
+
+  <button class="delete-todo js-delete-todo"><img src="https://img.icons8.com/external-wanicon-lineal-wanicon/64/000000/external-delete-user-interface-wanicon-lineal-wanicon.png" class="delete-todo js-delete-todo alt="delete" id="delete"/></button>
+  
+  <img class="edit" src="https://img.icons8.com/external-dreamstale-lineal-dreamstale/32/000000/external-edit-interface-dreamstale-lineal-dreamstale-2.png" alt="edit" />
+  `;
+  if (item) {
+    list.replaceChild(htmlLi, item);
+  } else {
+    list.append(htmlLi);
+  }
 }
 
 function addTodo(text) {
@@ -31,6 +50,22 @@ function addTodo(text) {
   renderTodo(todo);
 }
 
+function toggleDone(key) {
+  const index = userInput.findIndex((item) => item.id === Number(key));
+  userInput[index].completed = !userInput[index].completed;
+  renderTodo(userInput[index]);
+}
+
+function deleteTodo(key) {
+  const index = userInput.findIndex((item) => item.id === Number(key));
+  const todo = {
+    deleted: true,
+    ...userInput[index],
+  };
+  userInput = userInput.filter((item) => item.id !== Number(key));
+  renderTodo(todo);
+}
+
 const form = document.querySelector('.input-container');
 
 form.addEventListener('submit', (event) => {
@@ -41,5 +76,50 @@ form.addEventListener('submit', (event) => {
     addTodo(text);
     input.value = '';
     input.focus();
+  }
+});
+
+const list = document.querySelector('.form-items');
+
+list.addEventListener('click', (event) => {
+  if (event.target.classList.contains('.ticked')) {
+    const itemKey = event.target;
+    console.log(itemKey);
+    toggleDone(itemKey);
+  }
+  if (event.target.classList.contains('js-delete-todo')) {
+    const itemKey = event.target.parentElement.parentElement.dataset.key;
+    deleteTodo(itemKey);
+  }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  const ref = localStorage.getItem('todoItemsRef');
+  if (ref) {
+    userInput = JSON.parse(ref);
+    userInput.forEach((t) => {
+      renderTodo(t);
+    });
+  }
+});
+
+list.addEventListener('click', (event) => {
+  const ref = JSON.parse(localStorage.getItem('todoItemsRef'));
+  const thisTarget = event.target;
+  if (thisTarget.className === 'edit') {
+    const currentText = thisTarget.parentElement.querySelector('.task').innerHTML;
+    const indexCurrentText = (ref.map((task) => task.listItem).indexOf(currentText));
+    const textPar = thisTarget.parentElement.querySelector('.task');
+    let changedText = '';
+    textPar.contentEditable = true;
+    textPar.addEventListener('keyup', () => {
+      changedText = textPar.innerHTML;
+      if (currentText !== changedText) {
+        ref[indexCurrentText].listItem = changedText;
+        userInput = ref.filter((input) => input.listItem !== currentText);
+        localStorage.setItem('todoItemsRef', JSON.stringify(userInput));
+      }
+    });
+    // console.log(userInput);
   }
 });
