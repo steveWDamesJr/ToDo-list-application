e0273647f2c0fc013b0035325692c656b4e97432
SirriRyisa <sirriryisa@gmail.com>
2022-02-04 21:11:12 +0100

    Checked DOM update

--- src/__tests__/todo.test.js
+++ src/__tests__/todo.test.js
@@ -2,10 +2,10 @@
  * @jest-environment jsdom
  */
 
-// import renderTodo from '../modules/status.js';
+import renderTodo from '../modules/status.js';
 import storageManager from '../main-files/storage.js';
 import add from '../main-files/add.js';
-// import remove from '../main-files/remove.js';
+import remove from '../main-files/remove.js';
 
 describe('Saving and displaying list from local storage', () => {
   test("Add data to local storage", () => {
@@ -16,4 +16,26 @@ describe('Saving and displaying list from local storage', () => {
     data = storageManager.getData();
     expect(data.length).toEqual(2);
   });
+
+  test("Check update of DOM", () => {
+    document.body.innerHTML = `
+    <div class="listElementContainer">
+        <ul class="form-items">
+        
+          <!-- ToDo List FROM JS -->
+
+      </ul>
+    </div>
+    `;
+    add('Go to food store');
+    add('Go to KFC');
+    add('Go to Beverage Place');
+    add('Go to beach');
+    add('Go to clean the car');
+    renderTodo();
+    const list = document.querySelector('.form-items');
+    expect(list.childElementCount).toBe(7);
+  });
+});
+
 });
